18fa13d355a8eb24c69b35af49092df48adb573c
gularyusubova <gyusubova19960@ada.edu.az>
2026-02-26 10:54:14 +0400

    Add EventServiceImpl:{getUpcomingEvents} implementation

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -101,8 +101,25 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+
     public List<Event> getUpcomingEvents() {
-        return List.of();
+
+        LocalDateTime now = LocalDateTime.now();
+
+
+
+        return eventRepository.findAll()
+
+                .stream()
+
+                .filter(e -> e != null && e.getEventDateTime() != null)
+
+                .filter(e -> e.getEventDateTime().isAfter(now))
+
+                .sorted((a, b) -> a.getEventDateTime().compareTo(b.getEventDateTime()))
+
+                .collect(Collectors.toList());
+
     }
 
     @Override
